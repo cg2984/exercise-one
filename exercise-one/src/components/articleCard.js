908357e351abe71a429aba1ffea0a4e72593d86c
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2020-02-05 10:20:41 -0500

    adding mapping

--- exercise-one/src/components/articleCard.js
+++ exercise-one/src/components/articleCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
-function ArticleCard(){
+//going to put a single property called props into the function. thats all it can take
+//that article is from the article listing file where we set 
+//since article is an object we can add the .stuff to pull info from a json file
+function ArticleCard({articleData}){
+	console.log('article card article', articleData);
 	return (
 		<article>
 			<div>
 				<img src="" alt=""/>
 			</div>
 			<div>
-				<h2>Article Title</h2>
+				<h2>{articleData.title}</h2>
 				<p> date</p>
-				<p> blurb</p>
+				<p>{articleData.blurb}</p>
 				<a href=""> read more </a>
 			</div>
 		</article>
--- exercise-one/src/pages/articleListing.js
+++ exercise-one/src/pages/articleListing.js
@@ -10,7 +10,17 @@ function ArticleListing(){
 				<h1>Articles</h1>
 			</header>
 			<main>
-				<ArticleCard/>
+				{Data.map((article, i) => (
+					<ArticleCard 
+						key={i} 
+						articleData={article}
+					/>
+				//map is like a for loop
+				//the first parameter is the thing(s) in the json file, in this case it is the object article
+				//the second parameter is the i which is the amount of thing(s) in the json file. the map function automatically knows the amount of objects called "article" and cycles through each of them automatically
+				//for each thing called article in the json file, the map function makes an article card in the main and links it to the data from that specific thing in the json file
+				//this data is now called articleData and can be applied in different parts of app
+				))}
 			</main>
 		</div>
 	);
