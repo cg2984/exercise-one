e568a94863f4ebacc0b696619b62b3eea3527aad
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2020-02-05 08:45:44 -0500

    just going over the different files

--- exercise-one/src/index.js
+++ exercise-one/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+//^ this is just a render function with 2 arguements, and its rendering at the root in the second one, which I guess is the place to render
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
