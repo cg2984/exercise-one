c874676ca34a95e9a7fd76cf36f21bf6358729c3
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2020-02-19 10:05:08 -0500

    fixed everything with the url

--- exercise-one/src/components/articleCard.js
+++ exercise-one/src/components/articleCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 //that article is from the article listing file where we set 
 //since article is an object we can add the .stuff to pull info from a json file
 function ArticleCard({articleData}){
-	console.log('article card article', articleData);
+	console.log('article id', articleData.id);
 	return (
 		<article className="articleCard">
 			<div className="articleCard__image">
@@ -13,7 +13,7 @@ function ArticleCard({articleData}){
 				<h2>{articleData.title}</h2>
 				<p> date</p>
 				<p>{articleData.blurb}</p>
-				<a href='article/${articleData.id}'> read more </a>
+				<a href={`article/${articleData.id}`}> read more </a>
 			</div>
 		</article>
 	);
