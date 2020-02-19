3f88a49a6d7905de5e77a69d870ddf344bad63ca
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2020-02-19 10:21:39 -0500

    testing out the article text change

---
+++ exercise-one/src/components/ArticleItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ArticleItem({data,type}){
+	console.log("data type", data, type);
+	if type === "p"{
+		return <p>{data}</p>
+	}
+	
+	if type === "h2"{
+		return <h2>{data}</h2>
+	}
+	return <p>{data}</p>
+}
+
+export default ArticleItem
